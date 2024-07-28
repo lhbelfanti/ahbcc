e10835099793f6a7b04683b2c1796185d145bfb0
Lucas <lhbelfanti@gmail.com>
2024-07-28 01:23:23 -0300

    ⚙️ Removed columns that can be derived from others

--- migrations/002_create_tweets_quotes_table.sql
+++ migrations/002_create_tweets_quotes_table.sql
@@ -1,8 +1,6 @@
 CREATE TABLE IF NOT EXISTS tweets_quotes (
     id            SERIAL PRIMARY KEY,
     is_a_reply    BOOLEAN NOT NULL,
-    has_text      BOOLEAN NOT NULL,
-    has_images    BOOLEAN NOT NULL,
     text_content  TEXT NULL,
     images        TEXT[] NULL
 );
@@ -10,7 +8,5 @@ CREATE TABLE IF NOT EXISTS tweets_quotes (
 COMMENT ON TABLE tweets_quotes                IS 'Contains the quotes of tweets scrapped by GoXCrap';
 COMMENT ON COLUMN tweets_quotes.id            IS 'Auto-incrementing ID of the quote, agnostic to business logic';
 COMMENT ON COLUMN tweets_quotes.is_a_reply    IS 'Boolean indicating if the quoted tweet is a reply to another tweet';
-COMMENT ON COLUMN tweets_quotes.has_text      IS 'Boolean indicating if the quoted tweet contains text';
-COMMENT ON COLUMN tweets_quotes.has_images    IS 'Boolean indicating if the quoted tweet contains images';
 COMMENT ON COLUMN tweets_quotes.text_content  IS 'Text content of the quoted tweet, if any';
 COMMENT ON COLUMN tweets_quotes.images        IS 'Array of image URLs associated with the quoted tweet, if any';
