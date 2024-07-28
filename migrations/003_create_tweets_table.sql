4f18ab43cc74f515aaca8117135d0282436f064e
Lucas <lhbelfanti@gmail.com>
2024-07-28 01:23:23 -0300

    ⚙️ Removed columns that can be derived from others

--- migrations/003_create_tweets_table.sql
+++ migrations/003_create_tweets_table.sql
@@ -3,11 +3,8 @@ CREATE TABLE IF NOT EXISTS tweets (
     hash                TEXT NOT NULL,
     posted_at           TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     is_a_reply          BOOLEAN NOT NULL,
-    has_text            BOOLEAN NOT NULL,
-    has_images          BOOLEAN NOT NULL,
     text_content        TEXT NULL,
     images              TEXT[] NULL,
-    has_quote           BOOLEAN NOT NULL,
     quote_id            INTEGER NULL,
     search_criteria_id  INTEGER NOT NULL,
 
@@ -20,10 +17,7 @@ COMMENT ON COLUMN tweets.id                     IS 'Auto-incrementing ID of the 
 COMMENT ON COLUMN tweets.hash                   IS 'Unique hash identifier for the tweet';
 COMMENT ON COLUMN tweets.posted_at              IS 'Timestamp indicating when the tweet was posted';
 COMMENT ON COLUMN tweets.is_a_reply             IS 'Boolean indicating if the tweet is a reply to another tweet';
-COMMENT ON COLUMN tweets.has_text               IS 'Boolean indicating if the tweet contains text';
-COMMENT ON COLUMN tweets.has_images             IS 'Boolean indicating if the tweet contains images';
 COMMENT ON COLUMN tweets.text_content           IS 'The text content of the tweet, if any';
 COMMENT ON COLUMN tweets.images                 IS 'Array of image URLs associated with the tweet, if any';
-COMMENT ON COLUMN tweets.has_quote              IS 'Boolean indicating if the tweet contains a quoted tweet';
 COMMENT ON COLUMN tweets.quote_id               IS 'Foreign key referencing the ID of the quoted tweet';
 COMMENT ON COLUMN tweets.search_criteria_id     IS 'Foreign key referencing the ID of the search criteria';
