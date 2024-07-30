62f29430e1de7e24f3d9df633fba09bf2e5a87f8
Lucas <lhbelfanti@gmail.com>
2024-07-29 21:54:36 -0300

    ⚙️ Added ID column (as PK) to the tweets table and implemented an unique constrain in the hash and search_criteria_id columns

--- migrations/003_create_tweets_table.sql
+++ migrations/003_create_tweets_table.sql
@@ -1,4 +1,5 @@
 CREATE TABLE IF NOT EXISTS tweets (
+    id                  SERIAL PRIMARY KEY,
     hash                TEXT NOT NULL,
     posted_at           TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     is_a_reply          BOOLEAN NOT NULL,
@@ -7,12 +8,13 @@ CREATE TABLE IF NOT EXISTS tweets (
     quote_id            INTEGER NULL,
     search_criteria_id  INTEGER NOT NULL,
 
-    CONSTRAINT pk_tweets PRIMARY KEY (hash, search_criteria_id),
+    CONSTRAINT uq_hash_search_criteria UNIQUE (hash, search_criteria_id),
     CONSTRAINT fk_quote_id FOREIGN KEY(quote_id) REFERENCES tweets_quotes(id),
     CONSTRAINT fk_search_criteria_id FOREIGN KEY(search_criteria_id) REFERENCES search_criteria(id)
 );
 
 COMMENT ON TABLE tweets                         IS 'Contains the tweets scrapped by GoXCrap';
+COMMENT ON COLUMN tweets.id                     IS 'Auto-incrementing ID of the tweet, agnostic to business logic';
 COMMENT ON COLUMN tweets.hash                   IS 'Unique hash identifier for the tweet. It is part of the primary key';
 COMMENT ON COLUMN tweets.posted_at              IS 'Timestamp indicating when the tweet was posted';
 COMMENT ON COLUMN tweets.is_a_reply             IS 'Boolean indicating if the tweet is a reply to another tweet';
