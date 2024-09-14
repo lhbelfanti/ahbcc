8ea774ccbe63081c7ff3cf86e58bc34ee26d8cbd
Lucas <lhbelfanti@gmail.com>
2024-09-14 01:56:34 -0300

    ⚡️ Added search_criteria_executions UPDATE query

---
+++ cmd/api/search/criteria/update_test.go
@@ -0,0 +1,39 @@
+package criteria_test
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/jackc/pgx/v5/pgconn"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+
+	"ahbcc/cmd/api/search/criteria"
+	"ahbcc/internal/database"
+)
+
+func TestUpdateExecution_success(t *testing.T) {
+	mockPostgresConnection := new(database.MockPostgresConnection)
+	mockPostgresConnection.On("Exec", mock.Anything, mock.Anything, mock.Anything).Return(pgconn.CommandTag{}, nil)
+
+	updateExecution := criteria.MakeUpdateExecution(mockPostgresConnection)
+
+	got := updateExecution(context.Background(), 1, "DONE")
+
+	assert.Nil(t, got)
+	mockPostgresConnection.AssertExpectations(t)
+}
+
+func TestUpdateExecution_failsWhenUpdateOperationThrowsError(t *testing.T) {
+	mockPostgresConnection := new(database.MockPostgresConnection)
+	mockPostgresConnection.On("Exec", mock.Anything, mock.Anything, mock.Anything).Return(pgconn.CommandTag{}, errors.New("failed to update execution"))
+
+	updateExecution := criteria.MakeUpdateExecution(mockPostgresConnection)
+
+	want := criteria.FailedToUpdateSearchCriteriaExecution
+	got := updateExecution(context.Background(), 1, "DONE")
+
+	assert.Equal(t, want, got)
+	mockPostgresConnection.AssertExpectations(t)
+}
