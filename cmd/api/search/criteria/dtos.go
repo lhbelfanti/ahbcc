f17065e2ba33a8dc033032482fa1f0d4ca588f4a
Lucas <lhbelfanti@gmail.com>
2024-09-14 01:28:09 -0300

    ⚡️ Added search_criteria_execution_days INSERT query

--- cmd/api/search/criteria/dtos.go
+++ cmd/api/search/criteria/dtos.go
@@ -1,7 +1,17 @@
 package criteria
 
-// ExecutionDTO represents a search criteria execution to be inserted into the 'search_criteria_executions' table
-type ExecutionDTO struct {
-	Status           string `json:"status"`
-	SearchCriteriaID int    `json:"search_criteria_id"`
-}
+type (
+	// ExecutionDTO represents a search criteria execution to be inserted into the 'search_criteria_executions' table
+	ExecutionDTO struct {
+		Status           string `json:"status"`
+		SearchCriteriaID int    `json:"search_criteria_id"`
+	}
+
+	// ExecutionDayDTO represents a search criteria execution dat to be inserted into the 'search_criteria_execution_days' table
+	ExecutionDayDTO struct {
+		ExecutionDate             string  `json:"execution_date"`
+		TweetsQuantity            int     `json:"tweets_quantity"`
+		ErrorReason               *string `json:"error_reason"`
+		SearchCriteriaExecutionID int     `json:"search_criteria_execution_id"`
+	}
+)
