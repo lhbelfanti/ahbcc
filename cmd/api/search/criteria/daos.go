43e59d8b53febb58a709f9544de80f92f21eed68
Lucas <lhbelfanti@gmail.com>
2024-09-28 01:40:44 -0300

    🛠️ Refactor: Resume function now calls EnqueueCriteria with the proper search criteria execution id

--- cmd/api/search/criteria/daos.go
+++ cmd/api/search/criteria/daos.go
@@ -27,6 +27,15 @@ type (
 		Status           string `json:"status"`
 		SearchCriteriaID int    `json:"search_criteria_id"`
 	}
+
+	// ExecutionDayDAO represents a search criteria execution day
+	ExecutionDayDAO struct {
+		ID                        int       `json:"id"`
+		ExecutionDate             time.Time `json:"execution_date"`
+		TweetsQuantity            int       `json:"tweets_quantity"`
+		ErrorReason               string    `json:"error_reason"`
+		SearchCriteriaExecutionID int       `json:"search_criteria_execution_id"`
+	}
 )
 
 const (
--- cmd/api/search/criteria/enqueue_test.go
+++ cmd/api/search/criteria/enqueue_test.go
@@ -85,15 +85,30 @@ func TestEnqueue_failsWhenEnqueueCriteriaThrowsError(t *testing.T) {
 	assert.Equal(t, want, got)
 }
 
-func TestResume_success(t *testing.T) {
+func TestResume_successWhenSelectLastDayExecutedReturnsAnExecutionDay(t *testing.T) {
 	mockSelectCriteriaByID := criteria.MockSelectByID(criteria.MockCriteriaDAO(), nil)
 	mockDate := time.Date(2024, time.September, 19, 0, 0, 0, 0, time.Local)
-	mockSelectLastDayExecutedByCriteriaID := criteria.MockSelectLastDayExecutedByCriteriaID(mockDate, nil)
+	mockExecutionDayDAO := criteria.ExecutionDayDAO{ExecutionDate: mockDate, SearchCriteriaExecutionID: 1}
+	mockSelectLastDayExecutedByCriteriaID := criteria.MockSelectLastDayExecutedByCriteriaID(mockExecutionDayDAO, nil)
+	mockSelectExecutionsByStatuses := criteria.MockSelectExecutionsByStatuses(criteria.MockExecutionsDAO(), nil)
 	mockEnqueueCriteria := scrapper.MockEnqueueCriteria(nil)
 
-	resumeCriteria := criteria.MakeResume(mockSelectCriteriaByID, mockSelectLastDayExecutedByCriteriaID, mockEnqueueCriteria)
+	resumeCriteria := criteria.MakeResume(mockSelectCriteriaByID, mockSelectLastDayExecutedByCriteriaID, mockSelectExecutionsByStatuses, mockEnqueueCriteria)
 
-	got := resumeCriteria(context.Background(), 1)
+	got := resumeCriteria(context.Background(), 2)
+
+	assert.Nil(t, got)
+}
+
+func TestResume_successWhenSelectLastDayExecutedDoesntReturnAnExecutionDay(t *testing.T) {
+	mockSelectCriteriaByID := criteria.MockSelectByID(criteria.MockCriteriaDAO(), nil)
+	mockSelectLastDayExecutedByCriteriaID := criteria.MockSelectLastDayExecutedByCriteriaID(criteria.ExecutionDayDAO{}, criteria.NoExecutionDaysFoundForTheGivenCriteriaID)
+	mockSelectExecutionsByStatuses := criteria.MockSelectExecutionsByStatuses(criteria.MockExecutionsDAO(), nil)
+	mockEnqueueCriteria := scrapper.MockEnqueueCriteria(nil)
+
+	resumeCriteria := criteria.MakeResume(mockSelectCriteriaByID, mockSelectLastDayExecutedByCriteriaID, mockSelectExecutionsByStatuses, mockEnqueueCriteria)
+
+	got := resumeCriteria(context.Background(), 2)
 
 	assert.Nil(t, got)
 }
@@ -101,26 +116,43 @@ func TestResume_success(t *testing.T) {
 func TestResume_failsWhenSelectCriteriaByIDThrowsError(t *testing.T) {
 	mockSelectCriteriaByID := criteria.MockSelectByID(criteria.MockCriteriaDAO(), errors.New("failed to execute select criteria by id"))
 	mockDate := time.Date(2024, time.September, 19, 0, 0, 0, 0, time.Local)
-	mockSelectLastDayExecutedByCriteriaID := criteria.MockSelectLastDayExecutedByCriteriaID(mockDate, nil)
+	mockExecutionDayDAO := criteria.ExecutionDayDAO{ExecutionDate: mockDate, SearchCriteriaExecutionID: 1}
+	mockSelectLastDayExecutedByCriteriaID := criteria.MockSelectLastDayExecutedByCriteriaID(mockExecutionDayDAO, nil)
+	mockSelectExecutionsByStatuses := criteria.MockSelectExecutionsByStatuses(criteria.MockExecutionsDAO(), nil)
 	mockEnqueueCriteria := scrapper.MockEnqueueCriteria(nil)
 
-	resumeCriteria := criteria.MakeResume(mockSelectCriteriaByID, mockSelectLastDayExecutedByCriteriaID, mockEnqueueCriteria)
+	resumeCriteria := criteria.MakeResume(mockSelectCriteriaByID, mockSelectLastDayExecutedByCriteriaID, mockSelectExecutionsByStatuses, mockEnqueueCriteria)
 
 	want := criteria.FailedToExecuteSelectCriteriaByID
-	got := resumeCriteria(context.Background(), 1)
+	got := resumeCriteria(context.Background(), 2)
 
 	assert.Equal(t, want, got)
 }
 
 func TestResume_failsWhenSelectLastDayExecutedByCriteriaThrowsError(t *testing.T) {
 	mockSelectCriteriaByID := criteria.MockSelectByID(criteria.MockCriteriaDAO(), nil)
-	mockSelectLastDayExecutedByCriteriaID := criteria.MockSelectLastDayExecutedByCriteriaID(time.Time{}, errors.New("failed to execute select last day executed by criteria id"))
+	mockSelectLastDayExecutedByCriteriaID := criteria.MockSelectLastDayExecutedByCriteriaID(criteria.ExecutionDayDAO{}, errors.New("failed to execute select last day executed by criteria id"))
+	mockSelectExecutionsByStatuses := criteria.MockSelectExecutionsByStatuses(criteria.MockExecutionsDAO(), nil)
 	mockEnqueueCriteria := scrapper.MockEnqueueCriteria(nil)
 
-	resumeCriteria := criteria.MakeResume(mockSelectCriteriaByID, mockSelectLastDayExecutedByCriteriaID, mockEnqueueCriteria)
+	resumeCriteria := criteria.MakeResume(mockSelectCriteriaByID, mockSelectLastDayExecutedByCriteriaID, mockSelectExecutionsByStatuses, mockEnqueueCriteria)
 
 	want := criteria.FailedToExecuteSelectLastDayExecutedByCriteriaID
-	got := resumeCriteria(context.Background(), 1)
+	got := resumeCriteria(context.Background(), 2)
+
+	assert.Equal(t, want, got)
+}
+
+func TestResume_failsWhenSelectExecutionsByStatusesThrowsError(t *testing.T) {
+	mockSelectCriteriaByID := criteria.MockSelectByID(criteria.MockCriteriaDAO(), nil)
+	mockSelectLastDayExecutedByCriteriaID := criteria.MockSelectLastDayExecutedByCriteriaID(criteria.ExecutionDayDAO{}, criteria.NoExecutionDaysFoundForTheGivenCriteriaID)
+	mockSelectExecutionsByStatuses := criteria.MockSelectExecutionsByStatuses(criteria.MockExecutionsDAO(), errors.New("failed to execute select executions by statuses"))
+	mockEnqueueCriteria := scrapper.MockEnqueueCriteria(nil)
+
+	resumeCriteria := criteria.MakeResume(mockSelectCriteriaByID, mockSelectLastDayExecutedByCriteriaID, mockSelectExecutionsByStatuses, mockEnqueueCriteria)
+
+	want := criteria.FailedToExecuteSelectExecutionsByStatuses
+	got := resumeCriteria(context.Background(), 2)
 
 	assert.Equal(t, want, got)
 }
@@ -128,13 +160,29 @@ func TestResume_failsWhenSelectLastDayExecutedByCriteriaThrowsError(t *testing.T
 func TestResume_failsWhenEnqueueCriteriaThrowsError(t *testing.T) {
 	mockSelectCriteriaByID := criteria.MockSelectByID(criteria.MockCriteriaDAO(), nil)
 	mockDate := time.Date(2024, time.September, 19, 0, 0, 0, 0, time.Local)
-	mockSelectLastDayExecutedByCriteriaID := criteria.MockSelectLastDayExecutedByCriteriaID(mockDate, nil)
+	mockExecutionDayDAO := criteria.ExecutionDayDAO{ExecutionDate: mockDate, SearchCriteriaExecutionID: 1}
+	mockSelectLastDayExecutedByCriteriaID := criteria.MockSelectLastDayExecutedByCriteriaID(mockExecutionDayDAO, nil)
+	mockSelectExecutionsByStatuses := criteria.MockSelectExecutionsByStatuses(criteria.MockExecutionsDAO(), nil)
 	mockEnqueueCriteria := scrapper.MockEnqueueCriteria(errors.New("failed to execute enqueue criteria"))
 
-	resumeCriteria := criteria.MakeResume(mockSelectCriteriaByID, mockSelectLastDayExecutedByCriteriaID, mockEnqueueCriteria)
+	resumeCriteria := criteria.MakeResume(mockSelectCriteriaByID, mockSelectLastDayExecutedByCriteriaID, mockSelectExecutionsByStatuses, mockEnqueueCriteria)
 
 	want := criteria.FailedToExecuteEnqueueCriteria
-	got := resumeCriteria(context.Background(), 1)
+	got := resumeCriteria(context.Background(), 2)
 
 	assert.Equal(t, want, got)
+}
+
+func TestResume_failsWhenSelectLastDayExecutedDoesntReturnAnExecutionDayAndTheExecutionsInTheDBDoesntBelongToTheCriteria(t *testing.T) {
+	mockSelectCriteriaByID := criteria.MockSelectByID(criteria.MockCriteriaDAO(), nil)
+	mockSelectLastDayExecutedByCriteriaID := criteria.MockSelectLastDayExecutedByCriteriaID(criteria.ExecutionDayDAO{}, criteria.NoExecutionDaysFoundForTheGivenCriteriaID)
+	mockSelectExecutionsByStatuses := criteria.MockSelectExecutionsByStatuses(criteria.MockExecutionsDAO(), nil)
+	mockEnqueueCriteria := scrapper.MockEnqueueCriteria(nil)
+
+	resumeCriteria := criteria.MakeResume(mockSelectCriteriaByID, mockSelectLastDayExecutedByCriteriaID, mockSelectExecutionsByStatuses, mockEnqueueCriteria)
+
+	want := criteria.FailedToRetrieveSearchCriteriaExecutionID
+	got := resumeCriteria(context.Background(), 9999) // some random number for a criteria that is not present in the DB
+
+	assert.Equal(t, got, want)
 }
