1d1742e65fcb4694c8f0a3e00ed5307a201cbb31
Lucas <lhbelfanti@gmail.com>
2024-11-18 03:20:02 -0300

    🧑‍💻 Removed useless criteria after the analysis of the first run of all the criteria

--- migrations/008_insert_criteria.sql
+++ migrations/008_insert_criteria.sql
@@ -6,18 +6,10 @@ VALUES
     ('Cocaine Search Criteria B', NULL, 'tomar una linea', NULL, NULL, 'es', '2006-01-01', CURRENT_DATE),
     ('Cocaine Search Criteria C', NULL, 'tomar una raya', NULL, NULL, 'es', '2006-01-01', CURRENT_DATE),
     ('Cocaine Search Criteria D', NULL, 'tomar merca', NULL, NULL, 'es', '2006-01-01', CURRENT_DATE),
-    ('Cocaine Search Criteria E', NULL, 'necesito un saque', NULL, NULL, 'es', '2006-01-01', CURRENT_DATE),
-    ('Cocaine Search Criteria F', NULL, 'volver a tomar merca', NULL, NULL, 'es', '2006-01-01', CURRENT_DATE),
     -- Marijuana Search Criteria
-    ('Marijuana Search Criteria A', NULL, 'fumar un porro', NULL, NULL, 'es', '2006-01-01', CURRENT_DATE),
-    ('Marijuana Search Criteria B', NULL, 'necesito fumar porro', NULL, NULL, 'es', '2006-01-01', CURRENT_DATE),
-    ('Marijuana Search Criteria C', NULL, 'un porro', NULL, NULL, 'es', '2006-01-01', CURRENT_DATE),
+    ('Marijuana Search Criteria A', NULL, 'un porro', NULL, NULL, 'es', '2006-01-01', CURRENT_DATE),
     -- Heroin Search Criteria
     ('Heroin Search Criteria A', NULL, 'inyectarme heroina', NULL, NULL, 'es', '2006-01-01', CURRENT_DATE),
     -- Ecstasy Search Criteria
-    ('Ecstasy Search Criteria A', NULL, 'Necesito éxtasis', NULL, NULL, 'es', '2006-01-01', CURRENT_DATE),
-    -- LSD Search Criteria
-    ('LSD Search Criteria A', NULL, 'lsd', NULL, NULL, 'es', '2006-01-01', CURRENT_DATE),
-    ('LSD Search Criteria B', NULL, 'trip de ácido', NULL, NULL, 'es', '2006-01-01', CURRENT_DATE);
-
+    ('Ecstasy Search Criteria A', NULL, 'Necesito éxtasis', NULL, NULL, 'es', '2006-01-01', CURRENT_DATE)
 
