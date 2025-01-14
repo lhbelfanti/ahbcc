8ce9540d33416b264083a527adb6dff5b2b93f2a
Lucas Belfanti <lhbelfanti@gmail.com>
2025-01-14 01:53:42 -0300

    ⚡️ Added Delete session function

--- cmd/api/user/session/errors.go
+++ cmd/api/user/session/errors.go
@@ -5,4 +5,5 @@ import "errors"
 var (
 	FailedToInsertUserSession     = errors.New("failed to insert user session")
 	FailedToCreatUserSessionToken = errors.New("failed to create user session token")
+	FailedToDeleteUserSession     = errors.New("failed to delete user session")
 )
