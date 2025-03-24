49b9a66df224f983b5104134e8c97a7cbcebe92e
Lucas Belfanti <lhbelfanti@gmail.com>
2025-03-24 19:10:25 -0300

    ⚙️ Removed user_id from categorized_tweets because the table will be user agnostic

--- migrations/008_create_categorized_tweets_table.sql
+++ migrations/008_create_categorized_tweets_table.sql
@@ -2,20 +2,16 @@
 CREATE TABLE IF NOT EXISTS categorized_tweets (
     id                SERIAL PRIMARY KEY,
     tweet_id          INTEGER NOT NULL,
-    user_id           INTEGER NOT NULL,
     adverse_behavior  BOOLEAN NOT NULL,
 
-    CONSTRAINT fk_tweet_id FOREIGN KEY(tweet_id) REFERENCES tweets(uuid),
-    CONSTRAINT fk_user_id FOREIGN KEY(user_id) REFERENCES users(id)
+    CONSTRAINT fk_tweet_id FOREIGN KEY(tweet_id) REFERENCES tweets(uuid)
 );
 
 -- Table indexes
 CREATE INDEX IF NOT EXISTS idx_categorized_tweets_tweet_id ON categorized_tweets(tweet_id);
-CREATE INDEX IF NOT EXISTS idx_categorized_tweets_user_id ON categorized_tweets(user_id);
 
 -- Table comments
-COMMENT ON TABLE categorized_tweets                     IS 'Contains the categorization of tweets by user for adverse behavior';
+COMMENT ON TABLE categorized_tweets                     IS 'Contains the categorization of tweets for adverse behavior';
 COMMENT ON COLUMN categorized_tweets.id                 IS 'Auto-incrementing ID of the categorization record, agnostic to business logic';
 COMMENT ON COLUMN categorized_tweets.tweet_id           IS 'Foreign key referencing the ID of the tweet';
-COMMENT ON COLUMN categorized_tweets.user_id            IS 'Foreign key referencing the ID of the user who categorized the tweet';
 COMMENT ON COLUMN categorized_tweets.adverse_behavior   IS 'Boolean indicating if the tweet exhibits adverse behavior as determined by the user';
