208592b59301f04715cb86440381eba00d6e4e64
Lucas Belfanti <lhbelfanti@gmail.com>
2025-04-02 13:12:34 -0300

    ⚡️ Added counts.Insert function

---
+++ cmd/api/tweets/counts/errors.go
@@ -0,0 +1,7 @@
+package counts
+
+import "errors"
+
+var (
+	FailedToInsertTweetsCounts = errors.New("failed to insert tweets counts")
+)
--- cmd/api/tweets/counts/mocks.go
+++ cmd/api/tweets/counts/mocks.go
@@ -1,25 +1,20 @@
 package counts
 
+// MockTweetsCountsDAO mocks a counts.DAO
+func MockTweetsCountsDAO(searchCriteriaID, year, month, total int) DAO {
+	return DAO{
+		SearchCriteriaID: searchCriteriaID,
+		Year:             year,
+		Month:            month,
+		Total:            total,
+	}
+}
+
 // MockTweetsCountsDAOSlice mocks a []counts.DAO
 func MockTweetsCountsDAOSlice() []DAO {
 	return []DAO{
-		{
-			SearchCriteriaID: 1,
-			Year:             2024,
-			Month:            9,
-			Total:            350,
-		},
-		{
-			SearchCriteriaID: 1,
-			Year:             2025,
-			Month:            1,
-			Total:            1000,
-		},
-		{
-			SearchCriteriaID: 1,
-			Year:             2025,
-			Month:            2,
-			Total:            500,
-		},
+		MockTweetsCountsDAO(1, 2024, 9, 350),
+		MockTweetsCountsDAO(1, 2025, 1, 1000),
+		MockTweetsCountsDAO(1, 2025, 2, 500),
 	}
 }
