c50c40b40302d934b9c1c3fb6293de631ee2c8f4
Lucas Belfanti <lhbelfanti@gmail.com>
2025-04-10 02:40:02 -0300

    ⚙️ Moved SelectMonthlyTweetsCountsByYearByCriteriaID to summary pkg

--- cmd/api/search/criteria/executions/summary/select.go
+++ cmd/api/search/criteria/executions/summary/select.go
@@ -10,8 +10,13 @@ import (
 	"ahbcc/internal/log"
 )
 
-// SelectIDBySearchCriteriaIDYearAndMonth returns the id of a tweets counts row, seeking by its search_criteria_id, tweets_year, and tweets_month
-type SelectIDBySearchCriteriaIDYearAndMonth func(ctx context.Context, searchCriteriaID, year, month int) (int, error)
+type (
+	// SelectIDBySearchCriteriaIDYearAndMonth returns the id of a tweets counts row, seeking by its search_criteria_id, tweets_year, and tweets_month
+	SelectIDBySearchCriteriaIDYearAndMonth func(ctx context.Context, searchCriteriaID, year, month int) (int, error)
+
+	// SelectMonthlyTweetsCountsByYearByCriteriaID returns the count of all the tweets (using the `tweets` table) for each year and month, seeking by search criteria ID
+	SelectMonthlyTweetsCountsByYearByCriteriaID func(ctx context.Context, criteriaID int) ([]DAO, error)
+)
 
 // MakeSelectIDBySearchCriteriaIDYearAndMonth creates a new SelectIDBySearchCriteriaIDYearAndMonth
 func MakeSelectIDBySearchCriteriaIDYearAndMonth(db database.Connection) SelectIDBySearchCriteriaIDYearAndMonth {
@@ -33,5 +38,41 @@ func MakeSelectIDBySearchCriteriaIDYearAndMonth(db database.Connection) SelectID
 		}
 
 		return searchCriteriaExecutionSummaryID, nil
+	}
+}
+
+// MakeSelectMonthlyTweetsCountsByYearByCriteriaID creates a new SelectMonthlyTweetCountByYearByCriteriaID
+func MakeSelectMonthlyTweetsCountsByYearByCriteriaID(db database.Connection, collectRows database.CollectRows[DAO]) SelectMonthlyTweetsCountsByYearByCriteriaID {
+	const query string = `
+		SELECT 
+			EXTRACT(YEAR FROM posted_at) AS posted_at_year,
+			LPAD(EXTRACT(MONTH FROM posted_at)::text, 2, '0') AS posted_at_month,
+			COUNT(*) AS quantity
+		FROM 
+			tweets
+		WHERE 
+			search_criteria_id = $1
+		GROUP BY 
+			posted_at_year,
+			posted_at_month
+		ORDER BY 
+			posted_at_year DESC,
+			posted_at_month DESC;
+	`
+
+	return func(ctx context.Context, criteriaID int) ([]DAO, error) {
+		rows, err := db.Query(ctx, query, criteriaID)
+		if err != nil {
+			log.Error(ctx, err.Error())
+			return nil, FailedToRetrieveMonthlyTweetsCountsByYear
+		}
+
+		tweetsCounts, err := collectRows(rows)
+		if err != nil {
+			log.Error(ctx, err.Error())
+			return nil, FailedToExecuteCollectRowsInSelectMonthlyTweetsCountsByYear
+		}
+
+		return tweetsCounts, nil
 	}
 }
--- cmd/api/tweets/errors.go
+++ cmd/api/tweets/errors.go
@@ -8,9 +8,7 @@ const (
 )
 
 var (
-	FailedToInsertTweets                                        = errors.New("failed to insert tweets")
-	MissingTweetID                                              = errors.New("missing tweet ID")
-	MissingTweetSearchCriteriaID                                = errors.New("missing tweet search criteria ID")
-	FailedToRetrieveMonthlyTweetsCountsByYear                   = errors.New("failed to retrieve monthly tweet count by year")
-	FailedToExecuteCollectRowsInSelectMonthlyTweetsCountsByYear = errors.New("failed to execute select monthly tweet count by year")
+	FailedToInsertTweets         = errors.New("failed to insert tweets")
+	MissingTweetID               = errors.New("missing tweet ID")
+	MissingTweetSearchCriteriaID = errors.New("missing tweet search criteria ID")
 )
