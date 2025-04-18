fc469e2125e4446c874e368112b22e95e4f72011
Lucas Belfanti <lhbelfanti@gmail.com>
2025-04-18 14:37:58 -0300

    ⚡️ Added session.SelectUserIDByToken function

--- cmd/api/user/session/errors.go
+++ cmd/api/user/session/errors.go
@@ -3,8 +3,10 @@ package session
 import "errors"
 
 var (
-	FailedToInsertUserSession     = errors.New("failed to insert user session")
-	FailedToCreatUserSessionToken = errors.New("failed to create user session token")
-	FailedToDeleteUserSession     = errors.New("failed to delete user session")
-	FailedToDeleteExpiredSessions = errors.New("failed to delete expired sessions")
+	FailedToInsertUserSession            = errors.New("failed to insert user session")
+	FailedToCreatUserSessionToken        = errors.New("failed to create user session token")
+	FailedToDeleteUserSession            = errors.New("failed to delete user session")
+	FailedToDeleteExpiredSessions        = errors.New("failed to delete expired sessions")
+	NoUserIDFoundForTheGivenToken        = errors.New("no user id found for the given token")
+	FailedToExecuteQueryToRetrieveUserID = errors.New("failed to execute query to retrieve user id")
 )
---
+++ cmd/api/user/session/select.go
@@ -0,0 +1,33 @@
+package session
+
+import (
+	"ahbcc/internal/database"
+	"ahbcc/internal/log"
+	"context"
+	"errors"
+	"github.com/jackc/pgx/v5"
+)
+
+type SelectUserIDByToken func(ctx context.Context, token string) (int, error)
+
+func MakeSelectUserIDByToken(db database.Connection) SelectUserIDByToken {
+	const query string = `
+		SELECT id
+		FROM users_sessions
+		WHERE token = $1;
+	`
+
+	return func(ctx context.Context, token string) (int, error) {
+		var userID int
+		err := db.QueryRow(ctx, query, token).Scan(&userID)
+		if errors.Is(err, pgx.ErrNoRows) {
+			log.Error(ctx, err.Error())
+			return 0, NoUserIDFoundForTheGivenToken
+		} else if err != nil {
+			log.Error(ctx, err.Error())
+			return 0, FailedToExecuteQueryToRetrieveUserID
+		}
+
+		return userID, nil
+	}
+}
